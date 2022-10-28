06d86b77b927b1974b58b9c241e88070301ad341
Carlos L Rivera Medina <carlos.riveramedina@socskm.onmicrosoft.com>
2022-10-28 15:30:52 -0400

    Updated gitignore file, packages.json, and index.js to facilitate prd deployment.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import RemedyNavbar from "./Navbar.js";
 import Home from "./Home.js";
 import IncidentManager from "./IncidentManager.js";
 import NCRProjectTracker from "./NCRProjectTracker.js";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { createRoot } from "react-dom/client";
 import "./styles.css";
 import { ToastProvider } from "react-toast-notifications";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <div>
-    <Router basename="/">
-      <RemedyNavbar />
-      <ToastProvider>
-        <Routes>
-          <Route path="/*" element={<Home />} />
-          <Route path="/IncidentManager" element={<IncidentManager />} />
-          <Route path="/NCRProjectTracker" element={<NCRProjectTracker />} />
-        </Routes>
-      </ToastProvider>
-    </Router>
-  </div>
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <Router basename={process.env.PUBLIC_URL}>
+        <RemedyNavbar />
+        <ToastProvider>
+          <Routes>
+            <Route path="/*" element={<Home />} />
+            <Route path="/IncidentManager" element={<IncidentManager />} />
+            <Route path="/NCRProjectTracker" element={<NCRProjectTracker />} />
+          </Routes>
+        </ToastProvider>
+      </Router>
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
